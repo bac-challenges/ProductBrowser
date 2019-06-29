8d6ab23e6d41d4db628a867778fb4ff1c9762ae8
emile <emile@blueantcorp.com>
2019-06-29 18:32:49 +0300

    Initial push

---
+++ ProductBrowserModel/ProductBrowserModel.h
@@ -0,0 +1,42 @@
+//	MIT License
+//
+//	Copyright © 2019 Emile, Blue Ant Corp
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in all
+//	copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//	SOFTWARE.
+//
+//	ID: 1F59FD5C-8743-4E8F-9A91-26FC1305B536
+//
+//	Pkg: ProductBrowserModel
+//
+//	Swift: 5.0 
+//
+//	MacOS: 10.15
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProductBrowserModel.
+FOUNDATION_EXPORT double ProductBrowserModelVersionNumber;
+
+//! Project version string for ProductBrowserModel.
+FOUNDATION_EXPORT const unsigned char ProductBrowserModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProductBrowserModel/PublicHeader.h>
+
+
